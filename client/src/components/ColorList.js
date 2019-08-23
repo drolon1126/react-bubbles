69547e4a2b022bb8dcff24b1a335954b00c533d9
danRolon <daniel.rolon@yahoo.com>
2019-08-23 11:15:05 -0500

    stretch -set up add color functionality

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -9,12 +9,31 @@ const initialColor = {
 const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
+  const [adding, setAdding] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
   const editColor = color => {
     setEditing(true);
+    setAdding(false);
     setColorToEdit(color);
   };
+
+  const addColor = () => {
+    setAdding(true);
+    setColorToEdit(initialColor);
+  };
+
+  const addColorToList = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post(`http://localhost:5000/api/colors/`, colorToEdit)
+      .then(res=>{
+        console.log('add',res);
+        updateColors(res.data);
+        setAdding(false);
+      })
+
+  }
 
   const saveEdit = e => {
     e.preventDefault();
@@ -33,6 +52,7 @@ const ColorList = ({ colors, updateColors }) => {
           }
         })
         updateColors(tmp);
+        setEditing(false);
       })
       .catch(err => console.log("error", err.response));
   };
@@ -97,8 +117,40 @@ const ColorList = ({ colors, updateColors }) => {
           </div>
         </form>
       )}
+      <button onClick={addColor}>Add Color</button>
+      {adding && !editing && (
+        <form onSubmit={addColorToList}>
+          <legend>Add color</legend>
+          <label>
+            color name:
+            <input
+              onChange={e =>
+                setColorToEdit({ ...colorToEdit, color: e.target.value })
+              }
+              value={colorToEdit.color}
+            />
+          </label>
+          <label>
+            hex code:
+            <input
+              onChange={e =>
+                setColorToEdit({
+                  ...colorToEdit,
+                  code: { hex: e.target.value }
+                })
+              }
+              value={colorToEdit.code.hex}
+            />
+          </label>
+          <div className="button-row">
+            <button type="submit">Add</button>
+            <button onClick={() => setAdding(false)}>cancel</button>
+          </div>
+        </form>
+      )}
       <div className="spacer" />
       {/* stretch - build another form here to add a color */}
+      
     </div>
   );
 };
